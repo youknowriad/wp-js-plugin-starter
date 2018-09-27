c8ebc72fb563f05709abbdb2ac7ff883ab9961f8
Riad Benguella <benguella@gmail.com>
2018-09-27 09:03:42 +0100

    Step 3: Add a JS script, register and load it in WP Admin

--- wp-js-plugin-starter.php
+++ wp-js-plugin-starter.php
@@ -8,3 +8,48 @@
  *
  * @package wp-js-plugin-starter
  */
+
+ /**
+ * Retrieves a URL to a file in the plugin's directory.
+ *
+ * @param  string $path Relative path of the desired file.
+ *
+ * @return string       Fully qualified URL pointing to the desired file.
+ *
+ * @since 1.0.0
+ */
+function wp_js_plugin_starter_url( $path ) {
+	return plugins_url( $path, __FILE__ );
+}
+
+/**
+ * Registers the plugin's script.
+ *
+ * @since 1.0.0
+ */
+function wp_js_plugin_starter_register_script() {
+	wp_register_script(
+		'wp-js-plugin-starter',
+		wp_js_plugin_starter_url( 'src/index.js' ),
+		array()
+	);
+}
+
+/**
+ * Enqueues the plugin's script.
+ *
+ * @since 1.0.0
+ */
+function wp_js_plugin_starter_enqueue_script() {
+	wp_enqueue_script( 'wp-js-plugin-starter' );
+}
+
+/**
+ * Trigger the script registration on init.
+ */
+add_action( 'init', 'wp_js_plugin_starter_register_script' );
+
+/**
+ * Enqueue the script in all admin pages
+ */
+add_action( 'admin_enqueue_scripts', 'wp_js_plugin_starter_enqueue_script' );
