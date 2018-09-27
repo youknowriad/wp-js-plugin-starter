e88f6fc6e36ea14cb4631225c162905e31b57fc6
Riad Benguella <benguella@gmail.com>
2018-09-27 08:38:11 +0100

    Step 2: Create an empty WordPress plugin

---
+++ wp-js-plugin-starter.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WP JS Plugin Starter
+ * Plugin URI: https://github.com/youknowriad/wp-js-plugin-starter
+ * Description: Just another WordPress plugin starter
+ * Version: 1.0.0
+ * Author: Riad Benguella
+ *
+ * @package wp-js-plugin-starter
+ */
